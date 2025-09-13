86c31f2921368975ab0dcc4110da2320cf9f4370
ShwaTech <shwakesh13579@gmail.com>
2025-09-13 14:26:49 +0300

    Setup Completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'YouTubeSentiment',
+    version= '0.0.0',
+    author= 'ShwaTech',
+    author_email= 'shwakesh13579@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+)
